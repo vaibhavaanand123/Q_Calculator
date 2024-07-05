3afe18aaf1b5ce676e4f58690f43b9cf92987764
Vaibhav <vaibhavanandav@gmail.com>
2024-07-05 18:09:33 +0530

    Scientific Calculation update

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -3,9 +3,6 @@
  */
 package org.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 class AppTest {
     
   
--- app/src/test/java/org/example/ScientificCalculatorTest.java
+++ app/src/test/java/org/example/ScientificCalculatorTest.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.text.DecimalFormat;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
